17196e974610f0892662131f9c6c4919550b35cb
trueChazza <chaz.goodare@gmail.com>
2023-11-26 12:31:40 +1300

    initial commit

---
+++ lib/oapi_tmdb.ex
@@ -0,0 +1,18 @@
+defmodule OapiTmdb do
+  @moduledoc """
+  Documentation for `OapiTmdb`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> OapiTmdb.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/oapi_tmdb_test.exs
@@ -0,0 +1,8 @@
+defmodule OapiTmdbTest do
+  use ExUnit.Case
+  doctest OapiTmdb
+
+  test "greets the world" do
+    assert OapiTmdb.hello() == :world
+  end
+end

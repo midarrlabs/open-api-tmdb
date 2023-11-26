a9ab51e978cb3a99799da7c8d64b39d4ddfe2d80
trueChazza <chaz.goodare@gmail.com>
2023-11-26 13:21:35 +1300

    build: add meta

--- mix.exs
+++ mix.exs
@@ -7,7 +7,12 @@ defmodule OapiTmdb.MixProject do
       version: "0.1.0",
       elixir: "~> 1.15",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      description: "The Movie Database (TMDB) REST API client for Elixir",
+      package: [
+        licenses: ["MIT"],
+        links: %{"GitHub" => "https://github.com/midarrlabs/open-api-tmdb"}
+      ],
     ]
   end
 
@@ -21,8 +26,7 @@ defmodule OapiTmdb.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:ex_doc, "~> 0.27", only: :dev, runtime: false},
       {:oapi_generator, "0.1.0-rc.3", only: :dev, runtime: false}
     ]
   end

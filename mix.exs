095b91c5f3eeafde760667847261e563cdf9150a
trueChazza <chaz.goodare@gmail.com>
2023-11-26 13:02:05 +1300

    build: add oapi_generator

--- mix.exs
+++ mix.exs
@@ -23,6 +23,7 @@ defmodule OapiTmdb.MixProject do
     [
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:oapi_generator, "0.1.0-rc.3", only: :dev, runtime: false}
     ]
   end
 end

010fe4502812dd5c7d5c3505f35895625c35ab3f
trueChazza <chaz.goodare@gmail.com>
2024-01-27 11:36:43 +1300

    refactor: person get from request

--- lib/operations.ex
+++ lib/operations.ex
@@ -12,11 +12,7 @@ defmodule OapiTmdb.Operations do
   @spec person_details(integer) :: map
   def person_details(person_id) do
 
-    HTTPoison.request(%{
-      method: :get,
-      url: "https://api.themoviedb.org/3/person/#{person_id}",
-      params: %{"api_key" => Application.get_env(:oapi_tmdb, :api_key)}
-    })
+    HTTPoison.get!("https://api.themoviedb.org/3/person/#{person_id}?api_key=#{Application.get_env(:oapi_tmdb, :api_key)}")
     |> handle_response()
   end
 
